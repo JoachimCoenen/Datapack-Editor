a808147e171a0220d8b3b1ac71f08e8069f1e2ec
JoachimCoenen <joachimcoenen@icloud.com>
2023-10-14 09:27:21 +0200

    added hiddenimports for basePlugins and corePlugins

--- _hooks/hook-base.plugin.py
+++ _hooks/hook-base.plugin.py
@@ -1,9 +1,25 @@
 import os
+from os.path import abspath, dirname, join
 
-basePluginsResourcesPath = os.path.abspath(os.path.dirname(__file__) + '/../basePlugins/')
-corePluginsResourcesPath = os.path.abspath(os.path.dirname(__file__) + '/../corePlugins/')
+from PyInstaller.utils.hooks import collect_submodules
 
-datas = [
-    (basePluginsResourcesPath, 'basePlugins/'),
-    (corePluginsResourcesPath, 'corePlugins/'),
-]
+
+def getResourcesPath(name: str) -> str:
+	return abspath(dirname(__file__) + f'/../{name}/')
+
+
+def getDatas(name: str, path: str) -> list[tuple[str, str]]:
+	immediateResourceDirs = []
+
+	for dirName in next(os.walk(path), (None, (), None))[1]:
+		dirPath = join(path, dirName)
+		if dirName == 'resources':
+			immediateResourceDirs.append((dirPath, f'{name}/{dirName}/'))
+		else:
+			immediateResourceDirs += getDatas(f'{name}/{dirName}', dirPath)
+	return immediateResourceDirs
+
+
+pluginFolderNames = ['basePlugins', 'corePlugins']
+hiddenimports = [submodule for pluginFolderName in pluginFolderNames for submodule in collect_submodules(pluginFolderName)]
+datas = [resourceDir for pluginFolderName in pluginFolderNames for resourceDir in getDatas(pluginFolderName, getResourcesPath(pluginFolderName))]
