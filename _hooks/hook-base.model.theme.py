ea3c376ebc82e6c14c1bb60b9af7f2959c23e19a
JoachimCoenen <joachimcoenen@icloud.com>
2023-10-14 09:26:49 +0200

    fixed datas path for the default colorSchemes

--- _hooks/hook-base.model.theme.py
+++ _hooks/hook-base.model.theme.py
@@ -3,5 +3,5 @@ import os
 colorSchemesPath = os.path.abspath(os.path.dirname(__file__) + '/../colorSchemes/scheme_*.py')
 
 datas = [
-    (colorSchemesPath, 'gui/themes/colorSchemes/'),
+    (colorSchemesPath, 'base/model/colorSchemes/'),
 ]
