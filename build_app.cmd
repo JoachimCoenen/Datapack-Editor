a5189431a253cadb360cc90930880a0853008759
JoachimCoenen <joachimcoenen@icloud.com>
2023-10-21 17:56:14 +0200

    fixed build_app.cmd

--- build_app.cmd
+++ build_app.cmd
@@ -5,11 +5,10 @@ rem DANGER!:
 rmdir /s /q ".\build\"
 
 pyinstaller --onefile ..\main.py ^
--p ..\..\Cat ^
 --additional-hooks-dir ..\_hooks ^
 --hidden-import PyQt5.QtPrintSupport ^
 --exclude-module bprofile ^
---exclude-module Cat.utils.bprofileCustom ^
+--exclude-module cat.utils.bprofileCustom ^
 --exclude-module javalang ^
 --exclude-module numpy.distutils ^
 --exclude-module distutils ^
