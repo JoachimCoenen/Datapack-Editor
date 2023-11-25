d2b0f86ffc731287e439c4a132f3e24857ca3df7
JoachimCoenen <joachimcoenen@icloud.com>
2023-11-25 16:05:31 +0100

    Re: #42: updated MCFunctionSchema for 1.20.3 to snapshot 23w43a

--- corePlugins/datapackVersions/commands/v1_20_3_schema.py
+++ corePlugins/datapackVersions/commands/v1_20_3_schema.py
@@ -1,5 +1,5 @@
 """
-currently at minecraft version 1.20.3 (23w42a)
+currently at minecraft version 1.20.3 (23w43a)
 """
 
 import copy
@@ -7,19 +7,21 @@ import copy
 from cat.utils import first
 from cat.utils.collections_ import ChainedList
 from corePlugins.datapack.datapackContents import RESOURCES
+from corePlugins.mcFunction.argumentTypes import *
 from corePlugins.mcFunction.command import ArgumentSchema, COMMANDS_ROOT, CommandPartSchema, KeywordSchema, MCFunctionSchema, Options, TERMINAL
 from corePlugins.minecraft_data.fullData import FullMCData, getFullMcData
 from . import v1_20_2_schema
-from .argumentTypes import MINECRAFT_RESOURCE_LOCATION
+from .argumentTypes import *
+from .v1_20_2_schema import CommandsCreator
 
 
 def buildMCFunctionSchemas() -> dict[str, MCFunctionSchema]:
 	version1_20_3 = getFullMcData('1.20.3')
-	schema_1_20_3 = COMMANDS_V20.buildSchema(version1_20_3)
+	schema_1_20_3 = COMMANDS_V22.buildSchema(version1_20_3)
 	return {'Minecraft 1.20.3': schema_1_20_3}
 
 
-COMMANDS_V20: v1_20_2_schema.CommandsCreator = copy.deepcopy(v1_20_2_schema.COMMANDS)
+COMMANDS_V20: CommandsCreator = copy.deepcopy(v1_20_2_schema.COMMANDS)
 
 
 @COMMANDS_V20.modify(name='execute')
@@ -65,3 +67,72 @@ def modify_return_args(_: FullMCData, args: list[CommandPartSchema]) -> list[Com
 		next=Options([COMMANDS_ROOT])
 	))
 	return args
+
+
+COMMANDS_V22: CommandsCreator = copy.deepcopy(COMMANDS_V20)
+
+
+@COMMANDS_V20.add(
+	name='tick',
+	description="Control the ticking flow and measure the performance of the game. \nRequires elevated permissions (admins and above), and so it is not by default available in command blocks and data packs.",
+	opLevel=3
+)
+def build_tick_args(_: FullMCData) -> list[CommandPartSchema]:
+	return [
+		KeywordSchema(
+			name='query',
+			description="Outputs the current target ticking rate, with information about the tick times performance."
+		),
+		KeywordSchema(
+			name='rate ',
+			description="Sets a custom target ticking rate to the specified value. The value `rate` must be greater than `1.0` and lower than `10000.0`.",
+			next=Options([
+				ArgumentSchema(
+					name='rate',
+					type=BRIGADIER_FLOAT,
+					args=dict(min=1.0, max=10000.0)
+				)
+			])
+		),
+		KeywordSchema(
+			name='freeze',
+			description="Freezes all gameplay elements, except for players and any entity a player is riding."
+		),
+		KeywordSchema(
+			name='step',
+			description="",
+			next=Options([
+				ArgumentSchema(
+					name='time',
+					description="Runs the game for the specified number of ticks and then freezes the game again.This allows to step through the game a set amount of ticks at a time. Only works when the game is frozen.",
+					type=MINECRAFT_TIME,
+					args=dict(min=1, suggestions=[20])
+				),
+				KeywordSchema(
+					name='stop',
+					description="Stops the current stepping process, and re-freezes the game."
+				),
+			])
+		),
+		KeywordSchema(
+			name='unfreeze',
+			description="Unfreezes the game and resumes all gameplay elements."
+		),
+		KeywordSchema(
+			name='sprint',
+			description="",
+			next=Options([
+				ArgumentSchema(
+					name='time',
+					description="Runs the game while ignoring the set ticking target rate for the specified number of ticks. At the end of the sprint, the game will resume the previous ticking target and display performance information about the tick times while sprinting.",
+					type=MINECRAFT_TIME,
+					args=dict(min=1, suggestions=[20])
+				),
+				KeywordSchema(
+					name='stop',
+					description="Stops the current `/tick sprint`, and resumes the previous ticking target."
+				),
+			])
+		)
+	]
+
