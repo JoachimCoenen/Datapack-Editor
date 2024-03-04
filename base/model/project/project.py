d0a0c354f294ba43be62eb9e63dd6a4cea92da46
JoachimCoenen <joachimcoenen@icloud.com>
2024-03-04 09:29:51 +0100

    removed indexBundles field from repr

--- base/model/project/project.py
+++ base/model/project/project.py
@@ -319,7 +319,7 @@ class Root(SerializableDataclass):
 	_identifier: str = field(default='', metadata=catMeta(serializedName='identifier'))
 	"""Used for unique identification, of the root. Two DependencyDescr objects with the same identifier ALWAYS point o the same Root."""
 	dependencies: list[DependencyDescr] = field(default_factory=list, repr=False, compare=False, metadata=catMeta(serialize=False))
-	indexBundles: AspectDict[IndexBundleAspect] = field(default_factory=lambda: AspectDict(IndexBundleAspect), metadata=catMeta(serialize=False))
+	indexBundles: AspectDict[IndexBundleAspect] = field(default_factory=lambda: AspectDict(IndexBundleAspect), repr=False, compare=False, metadata=catMeta(serialize=False))
 
 	@property
 	def location(self) -> str:
