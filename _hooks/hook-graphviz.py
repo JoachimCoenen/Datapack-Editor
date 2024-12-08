d259839aaf4e6fd46eb8da29c57c27cebbea4e8c
JoachimCoenen <joachimcoenen@icloud.com>
2024-12-08 17:28:47 +0100

    fixed hook-graphviz.py

--- _hooks/hook-graphviz.py
+++ _hooks/hook-graphviz.py
@@ -54,20 +54,22 @@ if is_win:
 else:
     if is_linux:
         # see: https://stackoverflow.com/questions/78014447/pyinstaller-unable-to-find-usr-sbin-neato-when-adding-binary-and-data-files
-        graphviz_bindir = '/usr/bin'
+        # graphviz_bindir = '/usr/bin'
+        graphviz_bindir = os.path.dirname(shutil.which("dot"))
     else:
         # The dot binary in PATH is typically a symlink, handle that.
         # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/bin
         graphviz_bindir = os.path.dirname(os.path.realpath(shutil.which("dot")))
 
     for binary in progs:
-        binaries.append((graphviz_bindir + "/" + binary, "."))
+        binary_path = os.path.realpath(graphviz_bindir + "/" + binary)
+        binaries.append((binary_path, "."))
     if is_darwin:
         suffix = "dylib"
         # graphviz_libdir is e.g. /usr/local/Cellar/graphviz/2.46.0/lib/graphviz
         graphviz_libdir = os.path.realpath(graphviz_bindir + "/../lib/graphviz")
     else:
-        suffix = "so"
+        suffix = "so.6"  # suffix = "so"; strange, but the built DatapackEditor expects the libraries with a '.6' appended...
         # graphviz_libdir is e.g. /usr/lib64/graphviz
         graphviz_libdir = os.path.join(os.path.dirname(findLibrary('libcdt')), 'graphviz')
     for binary in glob.glob(graphviz_libdir + "/*." + suffix):
