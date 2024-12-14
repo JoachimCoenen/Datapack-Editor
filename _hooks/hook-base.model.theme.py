324aeb5805871b6bc3a59676883666ec6a20f6ec
JoachimCoenen <joachimcoenen@icloud.com>
2024-12-14 11:12:45 +0100

    removed numpy dependency for Default Dark color scheme

--- _hooks/hook-base.model.theme.py
+++ _hooks/hook-base.model.theme.py
@@ -1,7 +1,9 @@
 import os
 
 colorSchemesPath = os.path.abspath(os.path.dirname(__file__) + '/../colorSchemes/scheme_*.py')
+colorSchemesMinimizerPath = os.path.abspath(os.path.dirname(__file__) + '/../colorSchemes/minimizer.py')
 
 datas = [
     (colorSchemesPath, 'base/model/colorSchemes/'),
+    (colorSchemesMinimizerPath, 'base/model/colorSchemes/'),
 ]
